f87127ec774e24dce4848111773c7a7b101a320b
Devessier <baptiste@devessier.fr>
2021-02-23 00:25:45 +0100

    feat: init repl using node.js doc example

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,7 @@ module.exports = {
     ],
     rules: {
         indent: ['error', 4],
+        'no-restricted-syntax': 'off',
+        'no-console': 'off',
     },
 };
--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,30 @@
+import { createInterface } from 'readline';
+
 async function app() {
-    console.log('Taskmastersh');
+    console.log('Welcome to Taskmastersh!');
+
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: '$> ',
+    });
+
+    rl.prompt();
+
+    for await (const line of rl) {
+        switch (line.trim()) {
+        case 'hello':
+            console.log('world!');
+            break;
+        default:
+            console.log(`Say what? I might have heard '${line.trim()}'`);
+            break;
+        }
+
+        rl.prompt();
+    }
+
+    console.log('Have a great day!');
 }
 
 app().catch(console.error);
