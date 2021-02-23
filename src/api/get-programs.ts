465a18c3a8b27aff19d3cd06433aeb1d319d98ce
Devessier <baptiste@devessier.fr>
2021-02-23 13:49:26 +0100

    feat: show only program with name equal to argument

---
+++ src/api/get-programs.ts
@@ -0,0 +1,13 @@
+import { Fetcher, isGetAllProgramsResponse, Program } from '../types';
+
+export async function getAllPrograms(fetcher :Fetcher): Promise<Program[]> {
+    const response = await fetcher.get({
+        path: '/status',
+    });
+    const programs = await response.json();
+    if (!isGetAllProgramsResponse(programs)) {
+        throw new Error('Invalid content');
+    }
+
+    return programs.result.programs;
+}
--- src/commands.ts
+++ src/commands.ts
@@ -1,21 +1,82 @@
-import { ReplCommandArgs, isGetAllProgramsResponse } from './types';
+import {
+    Program, ProgramState, ReplCommandArgs,
+} from './types';
+import { getAllPrograms } from './api/get-programs';
+
+interface ProgramToPrint {
+    state: ProgramState
+    processesCount: number
+}
+
+interface ProcessToPrint {
+    id: string
+    pid: number
+    state: ProgramState
+}
+
+interface SingleProgramToPrint {
+    name: string
+    state: ProgramState
+    processes: ProcessToPrint[]
+}
+
+function mergeProgramsIntoObject(programs: Program[]): Record<string, ProgramToPrint> {
+    return programs.reduce<Record<string, ProgramToPrint>>(
+        (acc, { id, state, processes: { length: processesCount } }) => {
+            acc[id] = {
+                state,
+                processesCount,
+            };
+
+            return acc;
+        }, {},
+    );
+}
+
+function printPrograms(programs: Program[]) {
+    const programsToPrint = mergeProgramsIntoObject(programs);
+
+    console.dir(programsToPrint, {
+        depth: null,
+    });
+}
+
+function printSingleProgram(programs: Program[], programId: string) {
+    const program = programs.find(({ id }) => id === programId);
+    if (program === undefined) {
+        console.log(`Could not find program ${programId}`);
+        return;
+    }
+
+    const programToPrint: SingleProgramToPrint = {
+        name: programId,
+        processes: program.processes.map(({ id, pid, state }) => ({
+            id,
+            pid,
+            state,
+        })),
+        state: program.state,
+    };
+
+    console.dir(programToPrint, {
+        depth: null,
+    });
+}
 
 export async function statusCommand({
     context,
+    args,
 }: ReplCommandArgs): Promise<void> {
     const { fetcher } = context;
 
-    const response = await fetcher.get({
-        path: '/status',
-    });
+    const programs = await getAllPrograms(fetcher);
 
-    const content = await response.json();
+    if (typeof args[0] === 'string') {
+        const programId = args[0];
 
-    if (!isGetAllProgramsResponse(content)) {
-        throw new Error('Invalid content');
+        printSingleProgram(programs, programId);
+        return;
     }
 
-    console.dir(content.result.programs, {
-        depth: null,
-    });
+    printPrograms(programs);
 }
